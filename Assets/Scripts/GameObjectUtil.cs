495a40e1517a18d9ec1568024a2a00ee4b5d99bd
Ashton210z <ashton210z@gmail.com>
2016-10-10 14:28:39 -0700

    Super Zombie Runner 0.1a
    In Progress

---
+++ Assets/Scripts/GameObjectUtil.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameObjectUtil {
+
+    private static Dictionary<RecycleGameObject, ObjectPool> pools = new Dictionary<RecycleGameObject, ObjectPool>();
+
+
+    public static GameObject Instantiate(GameObject prefab, Vector3 pos)
+    {
+        GameObject instance = null;
+
+        instance = GameObject.Instantiate(prefab);
+        instance.transform.position = pos;
+
+        return instance;
+    }
+
+    public static void Destroy(GameObject gameObject)
+    {
+        var recycleGameObject = gameObject.GetComponent<RecycleGameObject>();
+
+        if (recycleGameObject != null)
+        {
+            recycleGameObject.Shutdown();
+        } else
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    private static Object GetObjectPool(RecycleGameObject reference)
+    {
+        ObjectPool pool = null;
+        if (pool.Containskey (reference))
+        {
+            pool = pools[reference];
+        } else
+        {
+            var poolContainer = 
+        }
+    }
+
+}
